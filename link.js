ce00f50830b7dfe56ef1cf65044fed8444301f45
SHUBHAM KUMAR VISHNOI <65655892+shubhamvishnoi3288@users.noreply.github.com>
2020-10-01 11:13:41 +0530

    My Calculater

---
+++ link.js
@@ -0,0 +1,23 @@
+function c(val)
+{
+     document.getElementById("d").value=val;
+}
+function v(val)
+{
+     document.getElementById("d").value+=val;
+}
+function e() 
+{ 
+  try 
+{ 
+     c(eval(document.getElementById("d").value)) 
+} 
+  catch(e) 
+{
+     c('Error') 
+} 
+}  
+
+
+
+
